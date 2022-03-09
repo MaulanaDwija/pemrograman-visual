527f35206a110701511d75ed725296dc3f8ae3cd
MaulanaDwija <72806527+MaulanaDwija@users.noreply.github.com>
2022-03-10 06:26:25 +0700

    Add files via upload

---
+++ Praktikum 1.py
@@ -0,0 +1,12 @@
+abjad = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+         'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+kata = input("Masukkan Kata = ")
+modulus = ""
+kunci = int(input("Masukkan Kunci = "))
+
+for x in range(len(kata)):
+    huruf = abjad.index(kata[x])+kunci
+    modulus = modulus+abjad[huruf % 26]
+for y in ""+modulus:
+    print(y)
